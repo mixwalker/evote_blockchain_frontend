060761067919b5ae70fe16ca37d6f9239be75ee6
MixWalker <mixwalkerz@gmail.com>
2023-01-08 16:58:56 +0700

    firstcommit

---
+++ src/app/app.module.ts
@@ -0,0 +1,71 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { BlockchainEvoteComponent } from './blockchain-evote/blockchain-evote.component';
+import { LogInComponent } from './blockchain-evote/log-in/log-in.component';
+import { CardModule } from 'primeng/card';
+import { ToastModule } from 'primeng/toast';
+import { InputTextModule } from 'primeng/inputtext';
+import { MenubarModule } from 'primeng/menubar';
+import { HomePageComponent } from './blockchain-evote/user/home-page/home-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
+import { MenuBarComponent } from './blockchain-evote/menu-bar/menu-bar.component';
+import { GalleriaModule } from 'primeng/galleria';
+import { FooterComponent } from './blockchain-evote/footer/footer.component';
+import { MenuModule } from 'primeng/menu';
+import { RegPresidentComponent } from './blockchain-evote/user/register/reg-president/reg-president.component';
+import { RegMemclubComponent } from './blockchain-evote/user/register/reg-memclub/reg-memclub.component';
+import { RegMemconcilComponent } from './blockchain-evote/user/register/reg-memconcil/reg-memconcil.component';
+import { PanelModule } from 'primeng/panel';
+import { CalendarModule } from 'primeng/calendar';
+import { TableModule } from 'primeng/table';
+import { FileUploadModule } from 'primeng/fileupload';
+import { RegisStatusComponent } from './blockchain-evote/user/regis-status/regis-status.component';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { VotingPageComponent } from './blockchain-evote/user/voting-page/voting-page.component';
+import { VotingScoreComponent } from './blockchain-evote/user/voting-score/voting-score.component';
+import { ChartModule } from 'primeng/chart';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlockchainEvoteComponent,
+    LogInComponent,
+    HomePageComponent,
+    MenuBarComponent,
+    FooterComponent,
+    RegPresidentComponent,
+    RegMemclubComponent,
+    RegMemconcilComponent,
+    RegisStatusComponent,
+    VotingPageComponent,
+    VotingScoreComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    CardModule,
+    ToastModule,
+    InputTextModule,
+    MenubarModule,
+    HttpClientModule,
+    GalleriaModule,
+    MenuModule,
+    PanelModule,
+    CalendarModule,
+    TableModule,
+    FileUploadModule,
+    ButtonModule,
+    SplitButtonModule,
+    ChartModule
+  ],
+  providers: [MessageService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/blockchain-evote/blockchain-evote.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blockchain-evote',
+  templateUrl: './blockchain-evote.component.html',
+  styleUrls: ['./blockchain-evote.component.scss']
+})
+export class BlockchainEvoteComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
