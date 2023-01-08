ec9ef820a61b599ab401a06d49bb1c128cc638a7
MixWalker <mixwalkerz@gmail.com>
2023-01-08 17:42:36 +0700

    test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BlockchainEvoteUI
+# EvoteBlockchainFrontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.10.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/blockchain-evote-ui'),
+      dir: require('path').join(__dirname, './coverage/evote_blockchain_frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
